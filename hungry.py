cc45f498b138288b30d503c343b8ed6201f1d7d3
sankaramanchikp <57783442+sankaramanchikp@users.noreply.github.com>
2021-02-14 19:56:00 -0800

    second version of hungry code

--- hungry.py
+++ hungry.py
@@ -1,1 +1,5 @@
-print("I'm hungry")
+ans = input("Are you hungry?")
+if ans=="Yes":
+    print("You just had biryani! How are you hungry again?")
+else:
+    print("Good. No food for you until dinner")
